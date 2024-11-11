f4f30bad61ee5ce79a3ea6958ec0d1eea1803206
asanjinez <alejandro.sanjinez1@gmail.com>
2024-11-11 18:51:43 -0300

    change default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { authGuard } from './core/guards/auth.guard';
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: 'auth/register',
+        redirectTo: 'auth/login',
         pathMatch: 'full'
     },
     {
